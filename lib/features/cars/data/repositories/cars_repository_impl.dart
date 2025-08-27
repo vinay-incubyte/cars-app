84b89f4fc7cf27941b15128ab331678517553855
vinay-incubyte <vinay@incubyte.co>
2025-08-27 13:54:34 +0530

    feat(data): cars repo impl - fetch cars Failure - test passed (GREEN)

--- lib/features/cars/data/repositories/cars_repository_impl.dart
+++ lib/features/cars/data/repositories/cars_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:cars_app/core/expections.dart';
 import 'package:cars_app/core/failure.dart';
 import 'package:cars_app/features/cars/data/data_sources/cars_remote_data_source.dart';
 import 'package:cars_app/features/cars/domain/entities/car_entity.dart';
@@ -10,6 +11,10 @@ class CarsRepositoryImpl implements CarsRepository {
 
   @override
   Future<Either<Failure, List<CarEntity>>> fetchCars() async {
-    return Right(await carsRemoteDataSource.fetchCars());
+    try {
+      return Right(await carsRemoteDataSource.fetchCars());
+    } on ServerException catch (_) {
+      return Left(ServerFailure(msg: "Server issue"));
+    }
   }
 }
