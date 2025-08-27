87ba7d79b1b5c8bfc69f32ab7eb5c2a9b235332f
vinay-incubyte <vinay@incubyte.co>
2025-08-27 13:07:13 +0530

    feat(data): implement Car Model from json (GREEN)

--- lib/features/cars/data/models/car_model.dart
+++ lib/features/cars/data/models/car_model.dart
@@ -11,7 +11,15 @@ class CarModel extends CarEntity {
     required super.image,
   });
 
-  factory CarModel.fromJson(Map<String,dynamic> json){
-    throw UnimplementedError();
+  factory CarModel.fromJson(Map<String, dynamic> json) {
+    return CarModel(
+      id: json['id'] as String,
+      name: json['vehicle'] ?? "",
+      manufacturer: json['manufacturer'] ?? "",
+      model: json['model'] ?? "",
+      fuel: json['fuel'] ?? "",
+      type: json['type'] ?? "",
+      image: json['image'] ?? "",
+    );
   }
 }
