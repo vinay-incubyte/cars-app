449e3bb5a566352d45bd64b3c2b613ab946cd4af
vinay-incubyte <vinay@incubyte.co>
2025-08-28 09:26:11 +0530

    test(presentation): verify cars page loader and list render- test failed (RED)

--- test/features/cars/presentation/view/cars_page_test.dart
+++ test/features/cars/presentation/view/cars_page_test.dart
@@ -1,9 +1,12 @@
+import 'package:cars_app/features/cars/data/models/car_model.dart';
 import 'package:cars_app/features/cars/domain/usecases/fetch_cars_usecase.dart';
 import 'package:cars_app/features/cars/presentation/cubit/cars_cubit.dart';
 import 'package:cars_app/features/cars/presentation/view/cars_view.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../cubit/cars_cubit_test.mocks.dart';
 
@@ -14,6 +17,22 @@ void main() {
   setUp(() {
     fetchCarsUsecase = MockFetchCarsUsecase();
     carsCubit = CarsCubit(fetchCarsUsecase: fetchCarsUsecase);
+    final carsList = List.generate(
+      10,
+      (index) => CarModel(
+        id: "${index + 1}",
+        name: 'Mercedes Benz Mercielago',
+        manufacturer: 'Mazda',
+        model: 'Cruze',
+        fuel: 'Gasoline',
+        type: 'Crew Cab Pickup',
+        image: 'http://www.regcheck.org.uk/image.aspx/@TWF6ZGEgQ3J1emU=',
+      ),
+    );
+    when(fetchCarsUsecase.call()).thenAnswer((_) async {
+      await Future.delayed(Duration.zero);
+      return Right(carsList);
+    });
   });
 
   group('verify cars page widgets', () {
@@ -27,10 +46,19 @@ void main() {
     testWidgets('verify cars page view title', (tester) async {
       // arrange
       await tester.pumpWidget(loadPageView());
-      // act
-
+      await tester.pumpAndSettle();
       // assert
       expect(find.text('Cars'), findsOneWidget);
+    });
+
+    testWidgets('verify loader and Cars list', (tester) async {
+      // arrange
+      await tester.pumpWidget(loadPageView());
+      await tester.pump();
+      // assert
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(find.byType(ListView), findsOneWidget);
     });
   });
 }
