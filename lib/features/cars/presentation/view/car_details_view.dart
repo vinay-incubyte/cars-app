5c3d1e372223b72ef750ea35312cdcda72cf6914
vinay-incubyte <vinay@incubyte.co>
2025-08-28 16:36:49 +0530

    feat(presentation): car details view - test passed (GREEN)

--- lib/features/cars/presentation/view/car_details_view.dart
+++ lib/features/cars/presentation/view/car_details_view.dart
@@ -1,12 +1,35 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cars_app/features/cars/domain/entities/car_entity.dart';
 import 'package:flutter/material.dart';
 
 class CarDetailsView extends StatelessWidget {
-  const CarDetailsView({super.key, required this.carEntity});
-  final CarEntity carEntity;
+  const CarDetailsView({super.key, required this.car});
+  final CarEntity car;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          car.name,
+          style: TextStyle(fontSize: 24),
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      body: Column(
+        children: [
+          CachedNetworkImage(
+            imageUrl: car.image,
+            fit: BoxFit.fitWidth,
+            height: 250,
+          ),
+          Text(car.name, style: TextStyle(fontSize: 22)),
+          Text("Manufacturer: ${car.manufacturer}"),
+          Text("Model: ${car.model}"),
+          Text("Type: ${car.type}"),
+          Text("Fuel type: ${car.fuel}"),
+        ],
+      ),
+    );
   }
 }
--- test/features/cars/presentation/view/car_details_page_test.dart
+++ test/features/cars/presentation/view/car_details_page_test.dart
@@ -1,22 +1,25 @@
-import 'dart:convert';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cars_app/features/cars/data/models/car_model.dart';
 import 'package:cars_app/features/cars/presentation/view/car_details_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../fixtures/fixture.dart';
-
 void main() {
   testWidgets('verify car details', (tester) async {
     // arrange
-    final carJson = jsonDecode(await Fixture.load('car_fixture.json'));
-    final car = CarModel.fromJson(carJson);
-    await tester.pumpWidget(MaterialApp(home: CarDetailsView(carEntity: car)));
+    final car = CarModel(
+      id: "1",
+      name: 'Mercedes Benz Mercielago',
+      manufacturer: 'Mazda',
+      model: 'Cruze',
+      fuel: 'Gasoline',
+      type: 'Crew Cab Pickup',
+      image: 'http://www.regcheck.org.uk/image.aspx/@TWF6ZGEgQ3J1emU=',
+    );
+    await tester.pumpWidget(MaterialApp(home: CarDetailsView(car: car)));
     await tester.pumpAndSettle();
     // assert
-    expect(find.byType(Text), findsExactly(5));
     expect(find.byType(CachedNetworkImage), findsOneWidget);
+    expect(find.byType(Text), findsExactly(6));
   });
 }
