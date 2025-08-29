563841d11d4a2780c6575172f6d621314d3d415b
vinay-incubyte <vinay@incubyte.co>
2025-08-29 11:26:33 +0530

    test(data): cars local data source - set cache - test failed (RED)

--- test/features/cars/data/data_sources/cars_local_data_source_test.dart
+++ test/features/cars/data/data_sources/cars_local_data_source_test.dart
@@ -12,27 +12,29 @@ import 'cars_local_data_source_test.mocks.dart';
 void main() {
   late CarsLocalDataSourceImpl carsLocalDataSourceImpl;
   late Database database;
+  late List<CarModel> cars;
 
   setUp(() {
     database = MockDatabase();
     carsLocalDataSourceImpl = CarsLocalDataSourceImpl(db: database);
+    cars = List.generate(
+      10,
+      (index) => CarModel(
+        id: "${index + 1}",
+        name: 'Mercedes Benz Mercielago',
+        manufacturer: 'Mazda',
+        model: 'Cruze',
+        fuel: 'Gasoline',
+        type: 'Crew Cab Pickup',
+        image: 'http://www.regcheck.org.uk/image.aspx/@TWF6ZGEgQ3J1emU=',
+      ),
+    );
   });
 
   group('Verify cars local storage data source', () {
     test('get cached cars if data exsit in DB', () async {
       // arrange
-      final List<CarModel> cars = List.generate(
-        10,
-        (index) => CarModel(
-          id: "${index + 1}",
-          name: 'Mercedes Benz Mercielago',
-          manufacturer: 'Mazda',
-          model: 'Cruze',
-          fuel: 'Gasoline',
-          type: 'Crew Cab Pickup',
-          image: 'http://www.regcheck.org.uk/image.aspx/@TWF6ZGEgQ3J1emU=',
-        ),
-      );
+
       final dbRows = cars.map((e) => e.toMap()).toList();
       when(database.query(CARS_TABLE)).thenAnswer((_) async => dbRows);
       // act
@@ -52,5 +54,45 @@ void main() {
       expect(() => call, throwsA(TypeMatcher<CacheException>()));
       verify(database.query(CARS_TABLE)).called(1);
     });
+
+    test('set cars cache with success', () async {
+      // arrange
+      when(database.delete(CARS_TABLE)).thenAnswer((_) async => 10);
+      final batch = _FakeBatch();
+      when(database.batch()).thenReturn(batch);
+      // act
+      await carsLocalDataSourceImpl.setCache(cars);
+      // assert
+      verify(database.delete(CARS_TABLE)).called(1);
+      verify(database.batch()).called(1);
+      expect(batch.inserts, cars.length);
+    });
   });
+}
+
+class _FakeBatch implements Batch {
+  int inserts = 0;
+
+  @override
+  Future<List<Object?>> commit({
+    bool? exclusive,
+    bool? noResult,
+    bool? continueOnError,
+  }) async {
+    return [];
+  }
+
+  @override
+  void insert(
+    String table,
+    Map<String, Object?> values, {
+    String? nullColumnHack,
+    ConflictAlgorithm? conflictAlgorithm,
+  }) {
+    inserts++;
+  }
+
+  // ignore unimplemented methods
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
