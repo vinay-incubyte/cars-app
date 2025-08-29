8984d3b8cf7abb7a4ba230d5f825a0ca6dbe2837
vinay-incubyte <vinay@incubyte.co>
2025-08-29 13:11:43 +0530

    feat(core): sql Cars db provider - test passed (GREEN)

--- lib/core/platforms/sql_db_provider.dart
+++ lib/core/platforms/sql_db_provider.dart
@@ -1,5 +1,37 @@
 import 'package:sqflite/sqflite.dart';
 
 abstract class SqlDbProvider {
-  Future<Database> getCarsDB();
+  Future<Database> getDB();
+}
+
+class CarDbProvider implements SqlDbProvider {
+  final DatabaseFactory dbFactory;
+  final String path;
+
+  CarDbProvider({DatabaseFactory? dbFactory, this.path = 'cars.db'})
+    : dbFactory = dbFactory ?? databaseFactory;
+
+  @override
+  Future<Database> getDB() async {
+    final db = await dbFactory.openDatabase(
+      path,
+      options: OpenDatabaseOptions(
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('''
+            CREATE TABLE cars(
+              id INTEGER PRIMARY KEY,
+              vehicle TEXT,
+              model TEXT,
+              type TEXT,
+              manufacturer TEXT,
+              fuel TEXT,
+              image TEXT
+            )
+          ''');
+        },
+      ),
+    );
+    return db;
+  }
 }
