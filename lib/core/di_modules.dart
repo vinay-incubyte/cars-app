c085149e0244c0c6552b010f45438630de87f013
vinay-incubyte <vinay@incubyte.co>
2025-08-29 15:51:17 +0530

    RED refactor(DI): DI config InternetConnectionChecker added - integration test failed

--- lib/core/di_modules.dart
+++ lib/core/di_modules.dart
@@ -2,6 +2,7 @@ import 'package:cars_app/core/base_url.dart';
 import 'package:cars_app/core/platforms/sql_db_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:sqflite/sqflite.dart';
 
 @module
@@ -12,4 +13,7 @@ abstract class RegisterModule {
   @lazySingleton
   @preResolve
   Future<Database> carsDB() async => await CarDbProvider().getDB();
+
+  @lazySingleton
+  InternetConnectionChecker get  internetConnectionChecker => InternetConnectionChecker.instance;
 }
