e7d9d1ee6bf1154c11203e34d8485b7ee04e3d53
vinay-incubyte <vinay@incubyte.co>
2025-08-29 09:56:37 +0530

    GREEN refactor(presentation): car details view test passed

--- lib/features/cars/presentation/view/car_details_view.dart
+++ lib/features/cars/presentation/view/car_details_view.dart
@@ -9,27 +9,46 @@ class CarDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          car.name,
-          style: TextStyle(fontSize: 24),
-          overflow: TextOverflow.ellipsis,
-        ),
-      ),
+      appBar: AppBar(),
       body: Column(
         children: [
-          CachedNetworkImage(
-            imageUrl: car.image,
-            fit: BoxFit.fitWidth,
-            height: 250,
+          Hero(
+            tag: car.id,
+            transitionOnUserGestures: true,
+            child: CachedNetworkImage(
+              imageUrl: car.image,
+              fit: BoxFit.fitWidth,
+              height: 250,
+              width: double.infinity,
+            ),
           ),
-          Text(car.name, style: TextStyle(fontSize: 22)),
-          Text("Manufacturer: ${car.manufacturer}"),
-          Text("Model: ${car.model}"),
-          Text("Type: ${car.type}"),
-          Text("Fuel type: ${car.fuel}"),
+          _buildInfo(),
         ],
       ),
     );
+  }
+
+  Widget _buildInfo() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 10,
+        children: [
+          Text(
+            car.name,
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          Text("Manufacturer : ${car.manufacturer}", style: _textStyle),
+          Text("Model : ${car.model}", style: _textStyle),
+          Text("Type : ${car.type}", style: _textStyle),
+          Text("Fuel type : ${car.fuel}", style: _textStyle),
+        ],
+      ),
+    );
+  }
+
+  TextStyle get _textStyle {
+    return TextStyle(fontSize: 20, fontWeight: FontWeight.w400);
   }
 }
