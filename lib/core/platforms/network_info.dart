a77d35d90fad58c5018400bc854fdeb5098d9bad
vinay-incubyte <vinay@incubyte.co>
2025-08-29 09:31:47 +0530

    feat(core): network info impl test passed (GREEN)

--- lib/core/platforms/network_info.dart
+++ lib/core/platforms/network_info.dart
@@ -7,10 +7,9 @@ abstract class NetworkInfo {
 class NetworkInfoImpl implements NetworkInfo {
   final InternetConnectionChecker connectionChecker;
   const NetworkInfoImpl({required this.connectionChecker});
-  
+
   @override
-  Future<bool> isConnected() {
-    // TODO: implement isConnected
-    throw UnimplementedError();
+  Future<bool> isConnected() async{
+    return await connectionChecker.hasConnection;
   }
 }
