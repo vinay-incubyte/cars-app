01428c67df3639d64435b849173ba9278a751943
vinay-incubyte <vinay@incubyte.co>
2025-09-03 14:02:21 +0530

    feat(data): PushNotificationRepositoryImpl requestPermission() throw Exception - test passed (GREEN)

--- lib/features/push_notification/data/repoistories/push_notification_repository_impl.dart
+++ lib/features/push_notification/data/repoistories/push_notification_repository_impl.dart
@@ -1,9 +1,12 @@
 import 'package:cars_app/core/failure.dart';
+import 'package:cars_app/core/mixns/logger_mixin.dart';
 import 'package:cars_app/features/push_notification/data/data_source/remote_push_notification_data_source.dart';
 import 'package:cars_app/features/push_notification/domain/respositories/push_notification_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class PushNotificationRepositoryImpl implements PushNotificationRepository {
+class PushNotificationRepositoryImpl
+    with LoggerMixin
+    implements PushNotificationRepository {
   final RemotePushNotificationDataSource remotePushNotificationDataSource;
 
   PushNotificationRepositoryImpl({
@@ -18,7 +21,14 @@ class PushNotificationRepositoryImpl implements PushNotificationRepository {
 
   @override
   Future<Either<Failure, bool>> requestPermission() async {
-    final status = await remotePushNotificationDataSource.requestPermission();
-    return status ? Right(status) : Left(PermissionFailure(msg: 'Not allowed'));
+    try {
+      final status = await remotePushNotificationDataSource.requestPermission();
+      return status
+          ? Right(status)
+          : Left(PermissionFailure(msg: 'Not allowed'));
+    } on Exception catch (e) {
+      debugLog("PushNotificationRepositoryImpl requestPermission() : $e");
+      return Left(PermissionFailure(msg: 'Not allowed'));
+    }
   }
 }
