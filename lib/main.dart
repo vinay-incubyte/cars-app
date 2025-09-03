87bf8dddfd2c8fb1680c26fe56f87e53e9c89023
vinay-incubyte <vinay@incubyte.co>
2025-09-03 10:47:45 +0530

    introduced: firebase core project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:cars_app/di.dart';
 import 'package:cars_app/features/cars/presentation/cubit/cars_cubit.dart';
+import 'package:cars_app/firebase_options.dart';
 import 'package:cars_app/routes/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await configureDependencies();
   runApp(const MainApp());
 }
