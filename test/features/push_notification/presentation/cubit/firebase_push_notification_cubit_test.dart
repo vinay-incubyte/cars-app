7571059ea4fcd7d522039a0d621541e747c50242
vinay-incubyte <vinay@incubyte.co>
2025-09-03 18:32:07 +0530

    test(presentation): FirebasePushNotificationCubit verify get FCM Token if permission failure - test failed (RED)

--- test/features/push_notification/presentation/cubit/firebase_push_notification_cubit_test.dart
+++ test/features/push_notification/presentation/cubit/firebase_push_notification_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:cars_app/core/failure.dart';
 import 'package:cars_app/features/push_notification/domain/usecases/get_fcm_token_usecase.dart';
 import 'package:cars_app/features/push_notification/domain/usecases/push_notification_permission_usecase.dart';
 import 'package:cars_app/features/push_notification/presentation/cubit/firebase_push_notification_cubit.dart';
@@ -32,6 +33,17 @@ void main() {
     test('verify permission request', () async {
       // arrange
       when(permissionUsecase.call()).thenAnswer((_) async => Right(true));
+      // act
+      await pushNotificationCubit.init();
+      // assert
+      verify(permissionUsecase.call()).called(1);
+    });
+
+    test('verify permission request failure', () async {
+      // arrange
+      when(
+        permissionUsecase.call(),
+      ).thenAnswer((_) async => Left(PermissionFailure(msg: 'not allowed')));
       // act
       await pushNotificationCubit.init();
       // assert
