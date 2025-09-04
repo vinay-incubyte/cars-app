1e2fb768eb0a053c044a36b012e4481a6ef37a56
vinay-incubyte <vinay@incubyte.co>
2025-09-04 17:25:26 +0530

    test(presentation):CarDetailsCubit get Car by Id Failure state - test failed (RED)

--- test/features/cars/presentation/cubit/car_details_cubit_test.dart
+++ test/features/cars/presentation/cubit/car_details_cubit_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cars_app/core/failure.dart';
 import 'package:cars_app/features/cars/data/models/car_model.dart';
 import 'package:cars_app/features/cars/domain/usecases/fetch_car_by_id_usecase.dart';
 import 'package:cars_app/features/cars/presentation/cubit/car_details_cubit.dart';
@@ -35,6 +36,23 @@ void main() {
       when(fetchCarByIdUsecase.call("0")).thenAnswer((_) async => Right(car));
       // assert
       final expected = [CarDetailsLoading(), CarDetailsLoaded(car: car)];
+      expectLater(carDetailsCubit.stream, emitsInOrder(expected));
+      // act
+      carDetailsCubit.fetchById("0");
+      // verify
+      verify(fetchCarByIdUsecase.call("0")).called(1);
+    });
+
+    test('verify get Car by Id Failure', () async {
+      // arrange
+      when(
+        fetchCarByIdUsecase.call("0"),
+      ).thenAnswer((_) async => Left(ServerFailure(msg: "Server issue")));
+      // assert
+      final expected = [
+        CarDetailsLoading(),
+        CarDetailsLoadError(error: "Server issue"),
+      ];
       expectLater(carDetailsCubit.stream, emitsInOrder(expected));
       // act
       carDetailsCubit.fetchById("0");
