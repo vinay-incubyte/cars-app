dff4381c9b5620dd68c5f89bc19037f6849c5f5f
vinay-incubyte <vinay@incubyte.co>
2025-09-05 13:12:09 +0530

    introduced(deep_link): CarDetailsView route config

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -5,21 +5,32 @@ import 'package:cars_app/features/cars/presentation/view/car_details_view.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
+    final uri = Uri.parse(settings.name ?? "/");
+
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'carDetails') {
+      final carId = uri.pathSegments.length > 1 ? uri.pathSegments[1] : null;
+      if (carId != null) {
+        return MaterialPageRoute(
+          builder: (_) {
+            return CarDetailsView(args: CarDetailsArgs(deepLinkId: carId));
+          },
+          settings: settings,
+        );
+      }
+    }
+
     switch (settings.name) {
       case AppRoutes.cars:
         return MaterialPageRoute(builder: (_) => CarsView());
 
       case AppRoutes.carDetails:
         final args = settings.arguments as CarDetailsArgs;
-        return MaterialPageRoute(
-          builder: (_) => CarDetailsView(args: args),
-        );
+        return MaterialPageRoute(builder: (_) => CarDetailsView(args: args));
 
       default:
         return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text('Unknown route')),
-          ),
+          builder: (_) =>
+              const Scaffold(body: Center(child: Text('Unknown route'))),
         );
     }
   }
